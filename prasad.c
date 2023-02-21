bcc2c1d092d361bf7cacb7d20b594c4b9eeb22cc
nSHREEPRASAD <125908206+nSHREEPRASAD@users.noreply.github.com>
2023-02-21 12:19:15 +0530

    Update prasad
    Update for sum.

--- prasad.c
+++ prasad.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
 
-int main(){
-    int i;
-    for (i=0; i<10; i++){
-        printf("%d", i);
+void main(){
+    int i,sum=0;
+    for (i=0; i<=10; i++){
+        sum=sum+i;
     }
-
-    return 0;
+    printf("%d",sum);
 }
 
 //yo oy 
